6da9d1f93e11268c278470b13a3ccf69b85a604b
tqhung <80533005+tqhung123456@users.noreply.github.com>
2024-06-10 17:50:01 +0700

    Update test.py

--- test.py
+++ test.py
@@ -33,8 +33,8 @@ relay7_OFF = [7, 6, 0, 0, 0, 137, 172]
 relay8_ON = [8, 6, 0, 0, 255, 201, 19]
 relay8_OFF = [8, 6, 0, 0, 0, 137, 83]
 
-soil_temperature = [10, 3, 0, 6, 0, 1, 101, 112]
-soil_moisture = [10, 3, 0, 7, 0, 1, 52, 176]
+soil_temperature = [9, 3, 0, 6, 0, 1, 101, 112]
+soil_moisture = [9, 3, 0, 7, 0, 1, 52, 176]
 
 # m485.modbus485_send(relay1_ON)
 # time.sleep(1)
